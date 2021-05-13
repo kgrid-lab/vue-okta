a19b2b98e49e168625b451f238441699479c876b
mradamgbeck <mradamgbeck@gmail.com>
2021-05-13 11:30:16 -0400

    added node server

--- src/config.js
+++ src/config.js
@@ -1,11 +1,8 @@
-import vueConfig from '../vue.config.js'
-
 export default {
   oidc: {
-    clientId: '0oaq8xytwLXhqbABm5d6',
+    clientId: '0oapdvd3osLL5SIZL5d6',
     issuer: 'https://dev-28324586.okta.com/oauth2/default',
-    redirectUri: window.location.origin + vueConfig.publicPath + '/login/callback',
+    redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email'],
-
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-okta/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 }
