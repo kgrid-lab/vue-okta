046a091667c05f251da0897c5f209505920c6a31
mradamgbeck <mradamgbeck@gmail.com>
2021-05-13 16:03:37 -0400

    corrected client id to heroku banana time

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   oidc: {
-    clientId: '0oapdvd3osLL5SIZL5d6',
+    clientId: '0oaqiy7l1lNUF4nDp5d6',
     issuer: 'https://dev-28324586.okta.com/oauth2/default',
     redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email'],
