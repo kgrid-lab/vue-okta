7281ff5326881e995dfc2940e9cbd59504604ffc
mradamgbeck <mradamgbeck@gmail.com>
2021-05-26 15:03:35 -0400

    switched to kgrid okta account

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   oidc: {
-    clientId: '0oaqiy7l1lNUF4nDp5d6',
-    issuer: 'https://dev-28324586.okta.com/oauth2/default',
+    clientId: '0oatjkd50WTpY3fsS5d6',
+    issuer: 'https://dev-53342887.okta.com/oauth2/default',
     redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email'],
   }
