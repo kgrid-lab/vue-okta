4cf0fd13a74cd133d19ea1ddc1e2fd24efc97287
mradamgbeck <mradamgbeck@gmail.com>
2021-06-02 11:17:12 -0400

    corrected client id

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   oidc: {
-    clientId: '0oatjkd50WTpY3fsS5d6',
+    clientId: '0oatjthnpG95NR1Na5d6',
     issuer: 'https://dev-53342887.okta.com/oauth2/default',
     redirectUri: window.location.origin + '/login/callback',
     scopes: ['openid', 'profile', 'email'],
